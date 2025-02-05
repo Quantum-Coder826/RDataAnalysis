03ff5f77b021ab7aba8d871283990764f6dcde2b
Quantum-Coder826 <b.veldthuis@gmail.com>
2025-02-05 10:17:50 +0100

    Compleate opdracht 4

--- Opdr4GroeisnelheidPlanten/GroeisnelheidPlanten.R
+++ Opdr4GroeisnelheidPlanten/GroeisnelheidPlanten.R
@@ -1,2 +1,21 @@
 library(tidyverse)
 
+plant_height <- read.csv("./plantheight.csv", sep = ";", dec = ",", header = TRUE)
+
+plant_height <- plant_height %>%
+  filter(Lux > 300) %>% # Lights under 300 lux have failed
+  filter(GrowthDuration >= 15) %>% # Planten moeten minstens 15 weeken groeien
+  mutate(gemiddeldeGroeiWeek = mean(TotalHeight) / GrowthDuration) # bereken gemiddelde groei per week per plant
+
+if (FALSE) { # Change to `FALSE` to select plot vraag e.
+  # plot vraag d
+  ggplot(plant_height, aes(x = GrowthDuration, y = TotalHeight, color = PlantSpecies)) +
+    geom_point()
+  
+} else {
+  # plot vraag e
+  ggplot(plant_height, aes(x = GrowthDuration, y = TotalHeight, 
+                           color = Lux, size = WaterLevel)) +
+    geom_point() +
+    facet_wrap(~ PlantSpecies) # '~' represents by
+}

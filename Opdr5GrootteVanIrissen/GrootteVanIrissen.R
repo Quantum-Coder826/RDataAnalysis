2ac4439033b911a4d8844b7a90a0cfb13bbe0fc4
Quantum-Coder826 <b.veldthuis@gmail.com>
2025-02-05 11:20:52 +0100

    Cleanup and added titles to plot, seartch plots using history

--- Opdr5GrootteVanIrissen/GrootteVanIrissen.R
+++ Opdr5GrootteVanIrissen/GrootteVanIrissen.R
@@ -6,16 +6,15 @@ means_iris <- iris %>%
   summarise(mean_sepal_length = mean(Sepal.Length),
             mean_sepal_width = mean(Sepal.Width))
 
-# Set if statements to `TRUE` to use the plot
-if (FALSE) { # vraag C
-  ggplot(iris, aes(x = Sepal.Width, y = Sepal.Length, colour = Species)) +
-    geom_point()
-}
+# vraag C
+ggplot(iris, aes(x = Sepal.Width, y = Sepal.Length, colour = Species)) +
+  geom_point() +
+  labs(title = "Vraag C")
 
-if (FALSE) { # vraag D
-  ggplot(iris, aes(x = Species, y = Sepal.Width, color = Species)) +
-    geom_boxplot()
-}
+# vraag D
+ggplot(iris, aes(x = Species, y = Sepal.Width, color = Species)) +
+  geom_boxplot() +
+  labs(title = "Vraag D")
 
 # vraag E
 iris %>%
@@ -28,7 +27,6 @@ iris %>%
 iris_setosa_virginica <- iris %>%
   filter(Species != "versicolor")
 
-if (TRUE) {
-  ggplot(iris_setosa_virginica, aes(x = Petal.Length, fill = Species)) + # use `fill` to set colors of boxes
-    geom_histogram(binwidth = 0.5)
-}
+ggplot(iris_setosa_virginica, aes(x = Petal.Length, fill = Species)) + # use `fill` to set colors of boxes
+  geom_histogram(binwidth = 0.5) +
+  labs(title = "Vraag F")

43d9a64baa627fd1c752c1b616b2e808a868bc04
Quantum-Coder826 <b.veldthuis@gmail.com>
2025-02-06 16:41:37 +0100

    stash progress'

--- Opdr9VisualisatieGrootteKelkbladeren/VisualisatieGrootteKelkbladeren.R
+++ Opdr9VisualisatieGrootteKelkbladeren/VisualisatieGrootteKelkbladeren.R
@@ -2,3 +2,11 @@ library(tidyverse)
 
 data("iris")
 str(iris)
+
+# Vraag B
+ggplot(iris, aes(Species, Sepal.Length, size = Sepal.Width)) +
+  geom_point(alpha = 0.4, position = position_jitterdodge(jitter.width = 0.3, 
+                                                          dodge.width = 0.3)) +
+  labs(title = "Vraag B", x = "soort", y = "Kelk blad lengte",
+       size = "Kelk blad breedte") +
+  theme()

8b84080c33af1de5b971a4389448fcd447959844
Quantum-Coder826 <b.veldthuis@gmail.com>
2025-02-06 08:44:49 +0100

    stash

--- Opdr2GroeisnelheidBacteriën/GroeisnelheidBacteriën.R
+++ Opdr2GroeisnelheidBacteriën/GroeisnelheidBacteriën.R
@@ -9,18 +9,4 @@ sum(bacteria$Bacteria == "Lactobacillus casei")
 
 sort(factor(bacteria$Temperature))
 
-resistent <- subset(bacteria, Antibiotic == TRUE)[,1:5]
-resistent
-
-# Vraag G
-Bacteria <- c("Bacillus subtilis", "Staphylococcus aureus", "Bacillus subtilis",
-              "Escherichia coli")
-GrowthRate <- c("0.2", "0.1", "0.3", "0.2")
-Temperature <- c("39", "35", "35", "38")
-Medium <- c("Agar", "Agar", "Broth", "Agar")
-pH <- c("3.3", "3.3", "3.3", "3.3")
-
-bacteria_zuur <- data.frame(Bacteria, GrowthRate, Temperature, Medium, pH) 
-
-bioreactor_data_list <- list(neutraal = bacteria, zuur = bacteria_zuur)
-bioreactor_data_list
+resistent <- bacteria[,order()]

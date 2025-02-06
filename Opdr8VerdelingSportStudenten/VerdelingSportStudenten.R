a6b62cbd5600f4369df5f7311374c4b902e15072
Quantum-Coder826 <b.veldthuis@gmail.com>
2025-02-06 11:13:36 +0100

    Vraag 8C voltooid

--- Opdr8VerdelingSportStudenten/VerdelingSportStudenten.R
+++ Opdr8VerdelingSportStudenten/VerdelingSportStudenten.R
@@ -1,5 +1,14 @@
 library(tidyverse)
 
 studenten_sport <- read.csv("studenten_sport.csv", sep = "&", dec = ",")
+
 studenten_sport$Studiejaar <- factor(studenten_sport$Studiejaar, 
-                                     levels = c("Eerstejaar", "Tweedejaar"), ordered = TRUE)
+                                     levels = c("Eerstejaar", "Tweedejaar", "Derdejaar", "Vierdejaar"),
+                                     ordered = TRUE)
+
+# Vraag C
+studenten_sport %>%
+  filter(Sportclub == "Ja") %>%
+  ggplot(aes(Studiejaar)) +
+    geom_bar()
+  

af4c636fa14dec0c983c718aa01c277da954c861
Quantum-Coder826 <b.veldthuis@gmail.com>
2025-02-06 19:32:36 +0100

    Compleate

--- Opdr9VisualisatieGrootteKelkbladeren/VisualisatieGrootteKelkbladeren.R
+++ Opdr9VisualisatieGrootteKelkbladeren/VisualisatieGrootteKelkbladeren.R
@@ -5,8 +5,9 @@ str(iris)
 
 # Vraag B
 ggplot(iris, aes(Species, Sepal.Length, size = Sepal.Width)) +
-  geom_point(alpha = 0.4, position = position_jitterdodge(jitter.width = 0.3, 
-                                                          dodge.width = 0.3)) +
+  geom_point(alpha = 0.4, color = "darkorchid3",
+             position = position_jitterdodge(jitter.width = 0.3, 
+                                             dodge.width = 0.3)) +
   labs(title = "Vraag B", x = "soort", y = "Kelk blad lengte",
        size = "Kelk blad breedte") +
-  theme()
+  theme_classic()

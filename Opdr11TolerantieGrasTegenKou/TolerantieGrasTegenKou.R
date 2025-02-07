154df74ca01e21a38fa1395712879716275e9be9
Quantum-Coder826 <b.veldthuis@gmail.com>
2025-02-07 14:40:20 +0100

    Compleat all:

--- Opdr11TolerantieGrasTegenKou/TolerantieGrasTegenKou.R
+++ Opdr11TolerantieGrasTegenKou/TolerantieGrasTegenKou.R
@@ -5,4 +5,11 @@ str(CO2)
 
 ggplot(CO2, aes(conc, uptake, colour = Treatment)) +
   geom_point() +
-  facet_grid(~ Plant)
+  facet_grid(~ factor(Plant))
+
+
+#vraag C
+ggplot(CO2, aes(conc, uptake, colour = Plant)) +
+  geom_line(linewidth = 1) +
+  facet_grid(Treatment ~ Type) +
+  scale_color_discrete()

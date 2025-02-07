179172e4596f3601c2cadb4a9e65e7ac9d69a7a3
Quantum-Coder826 <b.veldthuis@gmail.com>
2025-02-07 13:37:21 +0100

    Compleat Opdracht 8 vraag F

--- Opdr10GedragDierenInVerschillendeTemperatuurzones/GedragDierenInVerschillendeTemperatuurzones.R
+++ Opdr10GedragDierenInVerschillendeTemperatuurzones/GedragDierenInVerschillendeTemperatuurzones.R
@@ -20,3 +20,29 @@ ggplot(temperatuurzones, aes(Temperatuurzone, Activiteit, color = Soort)) +
   geom_jitter(width = 0.1) +
   labs(title = "Activiteit van verschillende soorten dieren bij verschillende temperatuurzones") +
   theme_minimal()
+
+# Vraag D
+ggplot(temperatuurzones, aes(Voedselconsumptie, fill = Temperatuurzone)) +
+  geom_histogram(binwidth = 50, center = 25) +
+  scale_fill_brewer("Pastel1") +
+  labs(title = "Vraag D", fill = "Temperatuurzone")
+
+# Vraag E
+temperatuurzones %>%
+  group_by(Temperatuurzone, Soort) %>%
+  summarise(GemActiviteit = mean(Activiteit), SEM = sd(Activiteit)/sqrt(n())) %>%
+  
+  ggplot(aes(x = Temperatuurzone, y = GemActiviteit, fill =
+                             Soort)) +
+    geom_col(position = "dodge") +
+    geom_errorbar(aes(ymin = GemActiviteit - SEM, ymax = GemActiviteit + SEM),
+                  position = position_dodge(0.9), width = 0.2)
+
+# Vraag F
+snelheden <- read.csv("Snelheden.csv")
+
+ggplot(snelheden, aes(Tijd, SnelheidGemiddeld)) +
+  geom_line(color = "darkolivegreen4") +
+  geom_ribbon(aes(ymin = SnelheidGemiddeld - SE, ymax = SnelheidGemiddeld + SE),
+              fill = "darkolivegreen4", alpha = 0.5)
+  
--- Opdr2GroeisnelheidBacteriën/GroeisnelheidBacteriën.R
+++ Opdr2GroeisnelheidBacteriën/GroeisnelheidBacteriën.R
@@ -2,8 +2,11 @@ library(readr)
 bacteria <- read_delim("bacteria.csv", delim = "@", 
                        escape_double = FALSE, trim_ws = TRUE)
 
+Rownames <- bacteria$ExperimentID
+
 # Remove first colum by selecting cols 2 to the end
 bacteria <- subset(bacteria, select = 2:7)
+rownames(bacteria) <- Rownames
 
 sum(bacteria$Bacteria == "Lactobacillus casei")
 
